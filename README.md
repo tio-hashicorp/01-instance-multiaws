4288e0323853444efdb838f8d7a446071a83da44
tiobagio <tio@hashicorp.com>
2023-08-29 17:01:29 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# 01-instance-multiaws: learn-terraform-dynamic-credentials
---
+++ main.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+ alias = "US"
+  region = var.aws_region
+}
+
+provider "aws" {
+  alias = "SG"
+  shared_config_files = [var.tfc_aws_dynamic_credentials.aliases["SG"].shared_config_file]
+}
+
+
+data "aws_ami" "amazon_linux" {
+  provider = aws.SG
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_instance" "web" {
+  provider = aws.SG
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = var.instance_type
+
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo yum update -y
+    sudo yum install httpd -y
+    sudo systemctl enable httpd
+    sudo systemctl start httpd
+    echo "<html><body><div>Hello, world!</div></body></html>" > /var/www/html/index.html
+    EOF
+
+  tags = var.tags
+}
