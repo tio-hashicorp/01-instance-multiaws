72f6888a9ac0bf5fc6d15f1181d3b6d9ce8687ae
Tio Bagio <10209203+tiobagio@users.noreply.github.com>
2023-11-30 22:53:45 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,26 @@
-provider "aws" {
-#  shared_config_files = [var.tfc_aws_dynamic_credentials.default.shared_config_file]
-  region = var.aws_region
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
 }
+
+provider "aws" {
+  region = var.aws_region
+  shared_config_files = [var.tfc_aws_dynamic_credentials.default.shared_config_file]
+}
+
+variable "tfc_aws_dynamic_credentials" {
+  description = "Object containing AWS dynamic credentials configuration"
+  type = object({
+    default = object({
+      shared_config_file = string
+    }) 
+  })
+}
+
 /*
 provider "aws" {
   alias = "SG"
